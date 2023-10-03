06660796ea2f71f8e2ac69bde86ded1d2ea96818
diana <diana-safina-2017@mail.ru>
2023-10-03 21:23:58 +0500

    homework3

--- src/utils.py
+++ src/utils.py
@@ -2,4 +2,4 @@ import os
 def get_key():
     env_var = os.environ
     env_var['YT_API_KEY'] = "AIzaSyAWvYvv5cT7KYlK-WtJSVA0CEfw_D02ZkE"
-    return os.getenv('YT_API_KEY', 'API_KEY_NOT_FOUND')
+    return os.getenv('YT_API_KEY')
